63617dd898dc6d35836f923c31274c1afa928b73
Michael Boulton <boulton@zoetrope.io>
2018-01-03 14:59:54 +0000

    Add option for other base repo in build script

--- buildall.sh
+++ buildall.sh
@@ -2,14 +2,16 @@
 
 set -ex
 
+BASEREPO=871947736413.dkr.ecr.eu-west-2.amazonaws.com/
+
 cd seed-mongo
-docker build -t 871947736413.dkr.ecr.eu-west-2.amazonaws.com/mockson-seed:0.1.2 .
-docker push 871947736413.dkr.ecr.eu-west-2.amazonaws.com/mockson-seed:0.1.2
+docker build -t ${BASEREPO}mockson-seed:0.1.2 .
+docker push ${BASEREPO}mockson-seed:0.1.2
 cd -
 
 ### Mockson webhooks container
 
 cd app
-docker build -t 871947736413.dkr.ecr.eu-west-2.amazonaws.com/mockson-webhooks:latest .
-docker push 871947736413.dkr.ecr.eu-west-2.amazonaws.com/mockson-webhooks:latest
+docker build -t ${BASEREPO}mockson-webhooks:latest .
+docker push ${BASEREPO}mockson-webhooks:latest
 cd -
