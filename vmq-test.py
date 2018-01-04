ade02a1bbe71cea84b1cad35d2882569c5f93ef4
Michael Boulton <boulton@zoetrope.io>
2018-01-04 09:58:24 +0000

    Make test script better

--- vmq-test.py
+++ vmq-test.py
@@ -1,13 +1,29 @@
 import paho.mqtt.client as mqtt
 import sys
 
+
+def _getdef(arg, default):
+    try:
+        return sys.argv[arg]
+    except IndexError:
+        return default
+
+
+user = _getdef(1, "use-token-auth")
+pw = _getdef(2, "-ankVuPqceD(LBd0Zc")
+sub = _getdef(3, "$SYS/#")
+client_id = _getdef(4, None)
+
+host = "mqtt.zoetrope.local"
+port = 80
+
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    sub = sys.argv[3] if len(sys.argv) > 3 else "$SYS/#"
     print("Subscribing to {}".format(sub))
     client.subscribe(sub)
 
@@ -15,12 +31,12 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-client = mqtt.Client(client_id=(sys.argv[1] if len(sys.argv) > 2 else "bob"))
+client = mqtt.Client(client_id=client_id, transport="websockets")
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.username_pw_set("use-token-auth", (sys.argv[2] if len(sys.argv) > 2 else "bob123"))
-client.connect("localhost", 1883, 60)
+client.username_pw_set("use-token-auth", pw)
+print(client.connect(host, port, 60))
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
