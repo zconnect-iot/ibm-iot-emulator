d26e933528b9eb7b2bdec556a756f08e5c1f5d04
Michael Boulton <boulton@zoetrope.io>
2018-01-19 11:38:40 +0000

    Update generation script to generate some data for the project too

--- seed-mongo/generate.py
+++ seed-mongo/generate.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python
 
+import uuid
 import bcrypt
 import json
 
 project = "abc123"
 
-def pwhash(string):
-    return bcrypt.hashpw(string.encode("utf8"), bcrypt.gensalt(prefix=b"2a")).decode("utf8")
+def create_user_data():
+    def pwhash(string):
+        return bcrypt.hashpw(string.encode("utf8"), bcrypt.gensalt(prefix=b"2a")).decode("utf8")
 
-data = [
-    {
-        "username": "v1:pid123:aircon:0xbeef",
-        "passhash": pwhash("abc"),
-        "client_id": "fk4opgk4pokwep4otk490tk34t",
-    },
-]
+    data = [
+        {
+            "username": "v1:pid123:aircon:0xbeef",
+            "passhash": pwhash("abc"),
+            "client_id": "fk4opgk4pokwep4otk490tk34t",
+        },
+    ]
 
-# TODO
-# call seed() so we dont have to do this
+    # TODO
+    # call seed() so we dont have to do this
 
-with open("acl.json", "w") as ofile:
-    ofile.write(json.dumps(data, indent=2))
+    with open("mqtt_user.json", "w") as ofile:
+        ofile.write(json.dumps(data, indent=2))
+
+
+def create_project_data():
+    project_key = uuid.uuid4()
+
+    data = [
+        {
+            "name": "CoolProject",
+            "project_keys": [
+                str(project_key),
+            ]
+        },
+    ]
+
+    with open("project.json", "w") as ofile:
+        ofile.write(json.dumps(data, indent=2))
+
+
+create_user_data()
+create_project_data()
