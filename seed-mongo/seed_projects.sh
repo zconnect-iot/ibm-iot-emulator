1d8d3c5389bdefd4a732bacd8473c1a07f217f6d
Michael Boulton <boulton@zoetrope.io>
2018-01-19 13:56:57 +0000

    Fix env vars

--- seed-mongo/seed_projects.sh
+++ seed-mongo/seed_projects.sh
@@ -5,15 +5,15 @@ sleep 1
 python3 seed.py \
     --host $MONGO_HOST \
     --db vmq_acl_auth \
+    --drop True \
     --datafile /mqtt_user.json \
     --ssl False \
-    --with-host-prefix True \
     --collection mqtt_user
 
 python3 seed.py \
     --host $MONGO_HOST \
     --db vmq_acl_auth \
+    --drop True \
     --datafile /project.json \
     --ssl False \
-    --with-host-prefix True \
     --collection project
