4cd25dafd6277fd27c141356744e699a4e1a8831
Michael Boulton <boulton@zoetrope.io>
2018-01-19 13:32:29 +0000

    Fix seeding

--- seed-mongo/seed_projects.sh
+++ seed-mongo/seed_projects.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-python generate.py
-
-python seed.py \
+python3 seed.py \
     --host $MONGO_HOST \
     --db vmq_acl_auth \
     --datafile mqtt_user.json \
     --collection mqtt_user
 
-python seed.py \
+python3 seed.py \
     --host $MONGO_HOST \
     --db vmq_acl_auth \
     --datafile project.json \
